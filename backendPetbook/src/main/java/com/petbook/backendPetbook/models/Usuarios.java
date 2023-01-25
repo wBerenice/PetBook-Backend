401b0be5216fa517238b956ec52601dbb8bfde9d
Gamaliel Sibaja <gamsibaja@gmail.com>
2023-01-25 16:41:07 -0600

    se agrego entidad usuario

--- backendPetbook/src/main/java/com/petbook/backendPetbook/models/Usuarios.java
+++ backendPetbook/src/main/java/com/petbook/backendPetbook/models/Usuarios.java
@@ -1,13 +1,86 @@
 package com.petbook.backendPetbook.models;
 
-import javax.annotation.Generated;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
 
 @Entity  
 @Table (name = "usuario")
 public class Usuarios {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	@Column ()
+	@Column (name= "id", unique=true, nullable=false)
+	private Long id;
+	@Column (nullable =false)
+	private String nombre;
+	private String correo;
+	private String especie;
+	private Long telefono;
+	private String contrasena;
+	
+	public Usuarios(Long id, String nombre, String correo, String especie, Long telefono, String contrasena) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.correo = correo;
+		this.especie = especie;
+		this.telefono = telefono;
+		this.contrasena = contrasena;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+
+	public Long getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(Long telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	
+	
+	
+	
 }
