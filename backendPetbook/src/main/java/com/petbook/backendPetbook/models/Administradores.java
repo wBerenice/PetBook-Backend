15028fde88b5f28bcb5932c7812c169719542b1a
BereniceSan <sanchezberenice09@gmail.com>
2023-01-25 17:04:30 -0600

    Se agregó entidad y repository de Administradores

---
+++ backendPetbook/src/main/java/com/petbook/backendPetbook/models/Administradores.java
@@ -0,0 +1,67 @@
+package com.petbook.backendPetbook.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "administradores")
+public class Administradores {
+	
+	public class administradores{
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		@Column(name = "id", unique = true, nullable = false)
+		private Long id;	
+		@Column(nullable = false)
+		private String nombre;
+		@Column(nullable = false)
+		private String contrasena;
+		
+		
+		public administradores() {
+			// TODO Auto-generated constructor stub
+		}
+
+
+		public administradores(Long id, String nombre, String contrasena) {
+			super();
+			this.id = id;
+			this.nombre = nombre;
+			this.contrasena = contrasena;
+		}
+
+
+		public String getNombre() {
+			return nombre;
+		}
+
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+
+		public String getContrasena() {
+			return contrasena;
+		}
+
+
+		public void setContrasena(String contrasena) {
+			this.contrasena = contrasena;
+		}
+
+
+		public Long getId() {
+			return id;
+		}
+		
+	
+	}//class Administradores
+
+
+
+}
---
+++ backendPetbook/src/main/java/com/petbook/backendPetbook/service/AdministradoresRepository.java
@@ -0,0 +1,9 @@
+package com.petbook.backendPetbook.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.petbook.backendPetbook.models.Administradores;
+
+public interface AdministradoresRepository extends JpaRepository<Administradores, Long>{
+
+}//interface AdministradoresRepository
