a86d1581c2350e0de4e3aa58a5ca48f8bbdec6ac
abrahamhorus <110262126+abrahamhorus@users.noreply.github.com>
2023-01-25 15:17:20 -0600

    se crea el proyecto en spring del backend

---
+++ backendPetbook/src/test/java/com/petbook/backendPetbook/BackendPetbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petbook.backendPetbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPetbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
