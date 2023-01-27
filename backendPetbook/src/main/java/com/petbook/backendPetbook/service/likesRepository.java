7c9c1fa3ff3a18799da5ef0a25db5698e21b0b6e
Gamaliel Sibaja <gamsibaja@gmail.com>
2023-01-27 15:54:48 -0600

    se agrego el service y el repository de likes

---
+++ backendPetbook/src/main/java/com/petbook/backendPetbook/service/likesRepository.java
@@ -0,0 +1,9 @@
+package com.petbook.backendPetbook.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.petbook.backendPetbook.models.Likes;
+
+public interface likesRepository extends JpaRepository<Likes, Long>{
+
+}// interface likesRepository
---
+++ backendPetbook/src/main/java/com/petbook/backendPetbook/service/likesService.java
@@ -0,0 +1,58 @@
+package com.petbook.backendPetbook.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.petbook.backendPetbook.models.Likes;
+
+@Service
+public class likesService {
+	private final likesRepository likesRepository;
+	@Autowired
+	public likesService(com.petbook.backendPetbook.service.likesRepository likesRepository) {
+		super();
+		this.likesRepository = likesRepository;
+	}//constructor
+	
+	public List<Likes> getAllLikes(){
+		return likesRepository.findAll();
+	}//getAllLikes
+	
+	public Likes getLikes (Long id) {
+		return likesRepository.findById(id).orElseThrow(
+				()-> new IllegalArgumentException("Los likes con el id: " + id + "no existen.")				
+				);
+	}//getLike
+	
+	public Likes deleteLikes (Long id) {
+		Likes tmp = null;
+		if (likesRepository.existsById(id)) {
+			tmp= likesRepository.findById(id).get();
+			likesRepository.deleteById(id);
+		}//if 
+		return tmp;
+	}//deleteLike
+	
+	public Likes addLikes(Likes like) {
+		return likesRepository.save(like);
+	}//addLike
+	
+	public Likes updateLikes (Long id, Long id_usuario, Long id_publicaciones) {
+		Likes tmp = null;
+		if (likesRepository.existsById(id)) {
+			tmp = likesRepository.findById(id).get();
+			if (id_usuario != null) tmp.setId_usuario(id_usuario);
+			if (id_publicaciones != null) tmp.setId_publicaciones(id_publicaciones);
+			likesRepository.save(tmp);
+		} else {
+			System.out.println("Update- Los likes con el id: " + id + "no existe");
+		}//if
+		return tmp;
+	}//updateLikes
+	
+	
+	
+	
+}// class likesService
