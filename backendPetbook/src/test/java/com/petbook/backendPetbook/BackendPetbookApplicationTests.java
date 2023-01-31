268379adfdbcf77b4d3b9b079ad680a8a15dc149
abrahamhorus <110262126+abrahamhorus@users.noreply.github.com>
2023-01-31 16:54:20 -0600

    Pruebas unitarias de administradores

--- backendPetbook/src/test/java/com/petbook/backendPetbook/BackendPetbookApplicationTests.java
+++ backendPetbook/src/test/java/com/petbook/backendPetbook/BackendPetbookApplicationTests.java
@@ -8,6 +8,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.petbook.backendPetbook.models.Administradores;
 import com.petbook.backendPetbook.models.Publicaciones;
 
 @SpringBootTest
@@ -76,4 +79,55 @@ public void pruebaGET() throws Exception {
 	.andExpect(content().string(containsString("El marco")));
 }//pruebaGet publicaciones
 
+
+
+//Pruebas Administradores
+
+@Test //Se debe implementar el @Test para indicar que es una prueba
+@DisplayName("Prueba el método post de administradores")
+public void pruebaPOST1() throws Exception {
+	Administradores a = new Administradores();
+	a.setNombre("lapepa");
+	a.setContrasena("pig");
+	
+	
+	this.mockMvc.perform(  post("/api/administradores/")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(asJsonString(a))
+			)
+	.andDo(print() )
+	.andExpect(status().isOk() )
+	.andExpect(content().string(containsString("Horus") ) );
+}//pruebapost administradores
+
+//pruebaGet
+@Test //Se debe implementar el @Test para indicar que es una prueba
+@DisplayName("Prueba el método GET de administradores")
+
+public void pruebaGET2() throws Exception {
+	this.mockMvc.perform(  get("/api/administradores/") )
+	.andDo(print() )
+	.andExpect(status().isOk() );
+	//.andExpect(content().string(containsString("Horus")));
+}//pruebaGet administradores
+
+@Test
+@DisplayName("Prueba del método delete en Admins")
+
+public void pruebaDELETE2() throws Exception{
+	this.mockMvc.perform(  delete("/api/administradores/2") )
+	.andDo(print() )
+	.andExpect(status().isOk() );
+		
+}//Prueba delete publicaciones
+
+@Test
+@DisplayName("Prueba el método UPDATE en administradores")
+
+public void pruebaUPDATE2() throws Exception{
+	this.mockMvc.perform(  put("/api/administradores/1").queryParam("contrasena", "elyoyo") )
+	.andDo(print() )
+	.andExpect(status().isOk() )
+	.andExpect(content().string(containsString("Horus")));
+}//UpdatePublicaciones
 }//Class
